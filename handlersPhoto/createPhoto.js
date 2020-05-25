19970b439b50f5594a5e62806cc49c489703a1f6
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-05-25 13:57:15 +0200

    add owner name to photo in db

--- handlersPhoto/createPhoto.js
+++ handlersPhoto/createPhoto.js
@@ -5,23 +5,50 @@ import dynamoDb from "../libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
     const eventList = event.Records || [];
-    const keyList = eventList.map(item => item.s3.object.key);
-
+    const keyList = eventList.map(item => decodeURIComponent(item.s3.object.key));
     const keyListLength = keyList.length;
+
+    let keyListByUser = {};
     for (let i = 0; i < keyListLength; i++) {
-        const key = decodeURIComponent(keyList[i]);
+        const key = keyList[i];
         const cognitoId = key.split('/')[1];
-        const params = {
+        let userKeyList = keyListByUser[cognitoId];
+        keyListByUser[cognitoId] = (userKeyList)?
+            [...userKeyList, key]
+            : [key];
+    }
+
+    const userList = Object.keys(keyListByUser);
+    const userListLength = userList.length;
+    for (let i = 0; i < userListLength; i++) {
+        const cognitoId = userList[i];
+        const userKeyList = keyListByUser[cognitoId];
+        const userParams = {
             TableName: process.env.photoTable,
             Item: {
-                PK: 'PO' + newPhotoId(),
+                PK: 'UBbase',
                 SK: 'U' + cognitoId,
-                url: key,
-                RND: RND(),
-                createdAt: now(),
             }
         };
-        await dynamoDb.put(params);
+        const result = await dynamoDb.get(userParams);
+        const user = result.Item;
+
+        const userKeyListLength = userKeyList.length;
+        for (let j = 0; j < userKeyListLength; j++) {
+            const key = userKeyList[j];            
+            const photoParams = {
+                TableName: process.env.photoTable,
+                Item: {
+                    PK: 'PO' + newPhotoId(),
+                    SK: 'U' + cognitoId,
+                    url: key,
+                    RND: RND(),
+                    createdAt: now(),
+                    owner: user.name,
+                }
+            };
+            await dynamoDb.put(photoParams);
+        }
     }
 
     return 'ok';
