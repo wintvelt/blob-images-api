32799e4c93ddaab732834854160e576617de22f3
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-05-26 20:37:29 +0200

    update memberships on profile change

--- handlersUser/updateUser.js
+++ handlersUser/updateUser.js
@@ -1,26 +1,56 @@
 import handler from "../libs/handler-lib";
-import dynamoDb from "../libs/dynamodb-lib";
+import dynamoDb, { getMemberships } from "../libs/dynamodb-lib";
+
+const memberUpdate = (PK, SK, newUser) => ({
+    Update: {
+        TableName: process.env.photoTable,
+        Key: {
+            PK,
+            SK,
+        },
+        UpdateExpression: "SET #user = :newUser",
+        ExpressionAttributeNames: {
+            '#user': 'user',
+        },
+        ExpressionAttributeValues: {
+            ":newuser": newUser,
+        },
+        ReturnValues: "ALL_NEW"
+    }
+});
 
 export const main = handler(async (event, context) => {
     const data = JSON.parse(event.body);
-    const params = {
+    const userId = 'U' + event.requestContext.identity.cognitoIdentityId;
+    const newUser = {
+        name: data.name || null,
+        avatar: data.avatar || null
+    };
+    const userParams = {
         TableName: process.env.photoTable,
         Key: {
             PK: 'UBbase',
-            SK: 'U' + event.requestContext.identity.cognitoIdentityId,
+            SK: userId,
         },
         UpdateExpression: "SET #name = :name, avatar = :avatar",
         ExpressionAttributeNames: {
             '#name': 'name',
         },
         ExpressionAttributeValues: {
-            ":name": data.name || null,
-            ":avatar": data.avatar || null
+            ":name": newUser.name,
+            ":avatar": newUser.avatar,
         },
         ReturnValues: "ALL_NEW"
     };
+    const memberships = getMemberships(userId);
+    const membershipUpdates = (await memberships).map(item => memberUpdate(item.PK, item.SK, newUser));
 
-    await dynamoDb.update(params);
+    await dynamoDb.transact({
+        TransactItems: [
+            { Update: userParams },
+            ...membershipUpdates
+        ]
+    });
 
     return { status: true };
 });
