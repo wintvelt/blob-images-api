45af5ec431a7d27b0a025f6b18781442e25ec824
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-05-26 18:34:22 +0200

    immediately return OK if id is new

--- handlersGroup/getGroup.js
+++ handlersGroup/getGroup.js
@@ -2,19 +2,19 @@ import handler from "../libs/handler-lib";
 import dynamoDb from "../libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
+    const groupId = event.pathParameters.id;
+    if (groupId === 'new') return '';
+
     const params = {
         TableName: process.env.photoTable,
         Key: {
             PK: 'GBbase',
-            SK: event.pathParameters.id,
+            SK: groupId,
         }
     };
-
     const result = await dynamoDb.get(params);
     if (!result.Item) {
         throw new Error("Item not found.");
     }
-
-    // Return the retrieved item
     return result.Item;
 });
