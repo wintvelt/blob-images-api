22adf77f8d38fb011eca1e6d6ff8b383cb03ebd6
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-06-03 12:56:17 +0200

    add option to create from filename

--- handlersAlbum/createAlbumPhoto.js
+++ handlersAlbum/createAlbumPhoto.js
@@ -1,5 +1,5 @@
 import handler from "../libs/handler-lib";
-import dynamoDb, { getMemberRole, getPhoto } from "../libs/dynamodb-lib";
+import dynamoDb, { getMemberRole, getPhoto, listPhotos } from "../libs/dynamodb-lib";
 import { now } from '../libs/helpers';
 
 export const main = handler(async (event, context) => {
@@ -7,18 +7,27 @@ export const main = handler(async (event, context) => {
     const groupId = event.pathParameters.id;
     const albumId = event.pathParameters.albumid;
     const data = JSON.parse(event.body);
+    const { photoId, filename } = data;
 
     const memberRole = await getMemberRole(userId, groupId);
     if (!memberRole) throw new Error('not a member of this group');
     const userIsAdmin = (memberRole === 'admin');
     if (!userIsAdmin) throw new Error('not authorized to add photos');
 
-    const photo = await getPhoto(data.photoId);
+    let photo;
+    if (photoId) {
+        photo = await getPhoto(photoId);
+    } else {
+        const photos = await listPhotos(userId);
+        photo = photos.find(p => p.image.includes(filename));
+    }
+    if (!photo) throw new Error('photo not found');
+
     const params = {
         TableName: process.env.photoTable,
         Item: {
             PK: `GP${groupId}#${albumId}`,
-            SK: data.photoId,
+            SK: photo.PK.slice(2),
             createdAt: now(),
             photo,
         }
