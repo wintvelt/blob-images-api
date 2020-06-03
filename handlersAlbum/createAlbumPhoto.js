078a675b72a405e77dd7da5f2124bf95585193f4
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-06-03 13:02:41 +0200

    make photo update in DB consistent

--- handlersAlbum/createAlbumPhoto.js
+++ handlersAlbum/createAlbumPhoto.js
@@ -1,5 +1,5 @@
 import handler from "../libs/handler-lib";
-import dynamoDb, { getMemberRole, getPhoto, listPhotos } from "../libs/dynamodb-lib";
+import dynamoDb, { getMemberRole, getPhoto } from "../libs/dynamodb-lib";
 import { now } from '../libs/helpers';
 
 export const main = handler(async (event, context) => {
@@ -18,8 +18,20 @@ export const main = handler(async (event, context) => {
     if (photoId) {
         photo = await getPhoto(photoId);
     } else {
-        const photos = await listPhotos(userId);
-        photo = photos.find(p => p.image.includes(filename));
+        const result = await dynamoDb.query({
+            TableName: process.env.photoTable,
+            IndexName: process.env.photoIndex,
+            KeyConditionExpression: "#u = :user and begins_with(PK, :p)",
+            ExpressionAttributeNames: {
+                '#u': 'SK',
+            },
+            ExpressionAttributeValues: {
+                ":user": userId,
+                ":p": 'PO',
+            },
+        });
+        const photos = result.Items;
+        photo = photos.find(p => p.url.includes(filename));
     }
     if (!photo) throw new Error('photo not found');
 
