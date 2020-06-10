37d4ae6e79982d428c061eed4951f232a7e8496a
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-06-10 15:47:17 +0200

    remove cognito authorizer

--- handlersInvite/publicGetInvite.js
+++ handlersInvite/publicGetInvite.js
@@ -1,27 +1,36 @@
 import handler from "../libs/handler-lib";
 import dynamoDb from "../libs/dynamodb-lib";
-import { btoa } from "../libs/helpers";
+import { btoa, expireDate, now } from "../libs/helpers";
 
 export const main = handler(async (event, context) => {
-    // const userId = event.requestContext && event.requestContext.identity &&
-    //     'U' + event.requestContext.identity.cognitoIdentityId;
+    const userId = 'U' + event.requestContext.identity.cognitoIdentityId;
+    let Key;
     try {
-        const Key = JSON.parse(btoa(event.pathParameters.id));
-        // check if invite is for user, if so: must be logged in user
-        const params = {
-            TableName: process.env.photoTable,
-            Key
-        };
-
-        const result = await dynamoDb.get(params);
-        if (!result.Item) {
-            throw new Error("Item not found.");
-        }
-        // check if invite is still valid
-
-        // Return the retrieved item
-        return result.Item;
-    } catch (error) {
-        throw new Error('invalid invite ID');
+        Key = JSON.parse(btoa(event.pathParameters.id));
+    } catch (_) {
+        throw new Error('invite ID invalid');
     }
+    // check if invite is for user, if so: must be logged in user
+    const inviteIsForAuthUser = (Key.PK.slice(0, 3) === 'UMU') && (!Key.PK.includes('@'));
+    if (inviteIsForAuthUser && Key.PK.slice(3) !== userId) throw new Error('invite not for you');
+
+    const params = {
+        TableName: process.env.photoTable,
+        Key
+    };
+
+    const result = await dynamoDb.get(params);
+    const invite = result.Item;
+    if (!invite) {
+        throw new Error("invite not found");
+    }
+    // check if invite is still an invite
+    if (invite.status !== 'invite') throw new Error('invite already accepted');
+
+    // check if invite is still valid
+    const expirationDate = expireDate(invite.createdAt);
+    if (now() > expirationDate) throw new Error('invite expired');
+
+    // Return the retrieved item
+    return invite;
 });
