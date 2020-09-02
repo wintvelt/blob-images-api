7b826ca579a00618657411d414c7a1cdf0447faf
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-09-02 22:29:36 +0200

    add deleteAlbumPhoto API

---
+++ handlersAlbum/deleteAlbumPhoto.js
@@ -0,0 +1,47 @@
+import handler from "../libs/handler-lib";
+import dynamoDb, { getMemberRole, getPhotoByUser } from "../libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    const userId = 'U' + event.requestContext.identity.cognitoIdentityId;
+    const groupId = event.pathParameters.id;
+    const albumId = event.pathParameters.albumid;
+    const data = JSON.parse(event.body);
+    const { photoId, filename } = data;
+
+    const memberRole = await getMemberRole(userId, groupId);
+    if (!memberRole) throw new Error('not a member of this group');
+    const userIsAdmin = (memberRole === 'admin');
+    if (!userIsAdmin) throw new Error('not authorized to add photos');
+
+    let photo;
+    if (photoId) {
+        photo = await getPhotoByUser(photoId, userId);
+    } else {
+        console.log(filename);
+        const result = await dynamoDb.query({
+            TableName: process.env.photoTable,
+            IndexName: process.env.photoIndex,
+            KeyConditionExpression: "#u = :user and begins_with(PK, :p)",
+            ExpressionAttributeNames: {
+                '#u': 'SK',
+            },
+            ExpressionAttributeValues: {
+                ":user": userId,
+                ":p": 'PO',
+            },
+        });
+        const photos = result.Items;
+        photo = photos.find(p => p.url.includes(filename));
+    }
+    if (!photo) throw new Error('photo not found');
+    const foundPhotoId = photo.PK.slice(2);
+
+    const result = await dynamoDb.delete({
+        TableName: process.env.photoTable,
+        Key: {
+            PK: `GP${groupId}#${albumId}`,
+            SK: foundPhotoId,
+        },
+    });
+    return 'ok';
+});
