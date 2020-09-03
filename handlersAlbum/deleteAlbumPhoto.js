411365375674f3f91382cc1bc1e7ba32481f8d68
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-09-03 16:36:55 +0200

    delete API without body

--- handlersAlbum/deleteAlbumPhoto.js
+++ handlersAlbum/deleteAlbumPhoto.js
@@ -1,47 +1,27 @@
 import handler from "../libs/handler-lib";
-import dynamoDb, { getMemberRole, getPhotoByUser } from "../libs/dynamodb-lib";
+import dynamoDb, { getMemberRole } from "../libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
     const userId = 'U' + event.requestContext.identity.cognitoIdentityId;
     const groupId = event.pathParameters.id;
     const albumId = event.pathParameters.albumid;
-    const data = JSON.parse(event.body);
-    const { photoId, filename } = data;
+    const photoId = event.pathParameters.photoid;
 
     const memberRole = await getMemberRole(userId, groupId);
     if (!memberRole) throw new Error('not a member of this group');
     const userIsAdmin = (memberRole === 'admin');
-    if (!userIsAdmin) throw new Error('not authorized to add photos');
-
-    let photo;
-    if (photoId) {
-        photo = await getPhotoByUser(photoId, userId);
-    } else {
-        console.log(filename);
-        const result = await dynamoDb.query({
-            TableName: process.env.photoTable,
-            IndexName: process.env.photoIndex,
-            KeyConditionExpression: "#u = :user and begins_with(PK, :p)",
-            ExpressionAttributeNames: {
-                '#u': 'SK',
-            },
-            ExpressionAttributeValues: {
-                ":user": userId,
-                ":p": 'PO',
-            },
-        });
-        const photos = result.Items;
-        photo = photos.find(p => p.url.includes(filename));
-    }
-    if (!photo) throw new Error('photo not found');
-    const foundPhotoId = photo.PK.slice(2);
+    if (!userIsAdmin) throw new Error('not authorized to remove photo from album');
 
     const result = await dynamoDb.delete({
         TableName: process.env.photoTable,
         Key: {
             PK: `GP${groupId}#${albumId}`,
-            SK: foundPhotoId,
+            SK: photoId,
         },
+        ReturnValues: 'ALL_OLD'
     });
+    console.log(result);
+    if (!result.Attributes) throw new Error('could not remove photo from album');
+
     return 'ok';
 });
--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -101,4 +101,23 @@ export const getPhotoByUser = async (photoId, userId) => {
     return result.Item;
 };
 
+export const getPhotoByGroupAlbum = async (photoId, groupId, albumId) => {
+    const params = {
+        TableName: process.env.photoTable,
+        Key: {
+            PK: `GA${groupId}#${albumId}`,
+            SK: photoId,
+        }
+    };
+
+    const result = await dynamoDb.get(params);
+    const photo = result.Item?.photo;
+    if (!photo) {
+        throw new Error("Photo not found.");
+    }
+
+    // Return the retrieved item
+    return photo;
+};
+
 export default dynamoDb;
