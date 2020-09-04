86a944af6317150c948be313ae8166429c60fb13
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-09-04 18:41:01 +0200

    add new photos to seenPics

--- handlersPhoto/createPhoto.js
+++ handlersPhoto/createPhoto.js
@@ -1,9 +1,11 @@
 // invoked from S3 Lambda trigger
 import { newPhotoId } from '../libs/helpers';
 import handler from "../libs/handler-lib";
+import dynamoDb from "../libs/dynamodb-lib";
 import { getUser, getMemberRole } from "../libs/dynamodb-lib-single";
 import s3 from "../libs/s3-lib";
 import { dbItem, dbCreate, dbCreateItem } from '../libs/dynamodb-create-lib';
+import { getMembers } from '../libs/dynamodb-query-lib';
 
 export const main = handler(async (event, context) => {
     const eventList = event.Records || [];
@@ -30,6 +32,7 @@ export const main = handler(async (event, context) => {
 
         const userKeyListLength = userKeyList.length;
         let createPromises = [];
+        let groupAlbums = {};
         for (let j = 0; j < userKeyListLength; j++) {
             const key = userKeyList[j];
             const photoId = newPhotoId();
@@ -58,9 +61,44 @@ export const main = handler(async (event, context) => {
                             compAfterType: `${groupid}#${albumid}`,
                         };
                         createPromises.push(dbCreateItem(AlbumPhotoItem));
+                        // add this photo groupAlbum list
+                        let photolist = groupAlbums[groupId] || new Set();
+                        photolist.add(`${albumid}#${photoId}`);
+                        groupAlbums[groupid] = photolist;
                     }
                 }
             };
+        }
+        // create updates for new photos in albums
+        const groupKeys = Object.keys(groupAlbums);
+        for (let i = 0; i < groupKeys.length; i++) {
+            const groupKey = groupKeys[i];
+            const newGroupAlbumPhotos = [...groupAlbums[groupKey]];
+            const newPics = newGroupAlbumPhotos.map(photo => ({ albumPhoto: photo }));
+            const members = await getMembers(groupKey);
+            for (let j = 0; j < members.length; j++) {
+                const member = members[j];
+                if (member.PK.slice(2) !== userId) {
+                    const oldSeenPics = member.seenPics || [];
+                    const oldSeenPicsKeys = oldSeenPics.map(it => it.albumPhoto);
+                    const newSeenPics = [
+                        ...oldSeenPics,
+                        ...newPics.filter(it => !oldSeenPicsKeys.includes(it.albumPhoto))
+                    ];
+                    const newPhotoUpdate = dynamoDb.update({
+                        TableName: process.env.photoTable,
+                        Key: {
+                            PK: member.PK,
+                            SK: member.SK
+                        },
+                        UpdateExpression: 'SET #s = :ns',
+                        ExpressionAttributeNames: { '#s': 'seenPics' },
+                        ExpressionAttributeValues: { ':ns': newSeenPics }
+                    });
+                    createPromises.push(newPhotoUpdate);
+                }
+            }
+
         }
         await Promise.all(createPromises);
     }
