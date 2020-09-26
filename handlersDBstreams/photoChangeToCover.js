583a1d101e959d0fe04f7e9003ced9490d44316c
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-09-26 15:30:37 +0200

    filter out also USER from cover update

--- handlersDBstreams/photoChangeToCover.js
+++ handlersDBstreams/photoChangeToCover.js
@@ -8,8 +8,9 @@ export const updateCoverPhoto = async (photo) => {
     let updatePromises = [];
     for (let j = 0; j < covers.length; j++) {
         const cover = covers[j];
-        if (cover.PK !== 'UBbase') {
+        if (cover.PK !== 'UBbase' && cover.PK !== 'USER') {
             // skip user records, to prevent circular update triggers photo->user->photo
+            // also: user cover only holds url and id - which never change
             const coverUpdate = dbUpdate(cover.PK, cover.SK, 'photo', photo);
             updatePromises.push(coverUpdate);
         }
