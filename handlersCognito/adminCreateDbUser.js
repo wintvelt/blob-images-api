4af2ed6221e531092b1282363b96562ec895b002
Wouter <wintvelt@me.com>
2020-10-30 17:34:08 +0100

    add zero photoStats too on creation

--- handlersCognito/adminCreateDbUser.js
+++ handlersCognito/adminCreateDbUser.js
@@ -1,11 +1,20 @@
 import { dbCreateItem } from 'blob-common/core/dbCreate';
 
 const createDbUser = async (request) => {
-    await dbCreateItem({
-        PK: 'UBbase',
-        SK: 'U'+request.userAttributes.sub,
-        email: request.userAttributes.email
-    });
+    const { sub, email } = request.userAttributes;
+
+    await Promise.all([
+        dbCreateItem({
+            PK: 'UBbase',
+            SK: 'U' + sub,
+            email: email
+        }),
+        dbCreateItem({
+            PK: 'UPstats',
+            SK: 'U' + sub,
+            photoCount: 0
+        })
+    ]);
 };
 export const adminCreateDbUser = (request) => {
     createDbUser(request);
