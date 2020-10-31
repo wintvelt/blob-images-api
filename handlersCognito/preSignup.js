edfd448029b5c00f9b13bf98ac02934ec5e85aa9
Wouter <wintvelt@me.com>
2020-10-31 18:17:36 +0100

    validate inviteId in database

--- handlersCognito/preSignup.js
+++ handlersCognito/preSignup.js
@@ -1,12 +1,35 @@
+import { btoa } from 'blob-common/core/base64';
+import { dynamoDb } from 'blob-common/core/db';
+
 const webmaster = process.env.webmaster || process.env.devWebmaster;
 const allowSignup = (process.env.devAllowSignup && process.env.devAllowSignup === 'true');
 
-export const main = (event, context, callback) => {
+const getInvite = async Key => {
+    const inviteResult = await dynamoDb.get({
+        Key
+    });
+    return inviteResult.Item;
+};
+
+export const main = async (event, context, callback) => {
     const email = event.request.userAttributes.email;
     if (allowSignup || email === webmaster) {
         callback(null, event);
     } else {
-        throw new Error('signup denied');
-        // callback(null, {});
+        const inviteId = event.request.validationData?.inviteId;
+        if (!inviteId) throw new Error('no invite ID provided');
+
+        let Key;
+        try {
+            Key = JSON.parse(btoa(inviteId));
+        } catch (_) {
+            throw new Error('invite ID invalid');
+        }
+
+        const invite = await getInvite(Key);
+        console.log({ invite });
+        if (!invite) throw new Error('invite not found');
+
+        callback(null, event);
     }
 };
