ea3181b5ec163aad113f611cf3aff54762231e02
Wouter <wintvelt@me.com>
2020-11-01 19:55:02 +0100

    fix signup

--- serverlessDomainConfig.js
+++ serverlessDomainConfig.js
@@ -1,12 +1,12 @@
 // to connect the dbStream function to the correct stream in dynamoDb
 module.exports.dynamoStream = () => ({
     'dev': 'arn:aws:dynamodb:eu-central-1:899888592127:table/blob-images-photos-dev/stream/2020-09-22T17:50:29.898',
-    'prod': 'arn:aws:dynamodb:eu-central-1:899888592127:table/blob-images-photos/stream/2020-09-09T17:57:49.872'
+    'prod': 'arn:aws:dynamodb:eu-central-1:899888592127:table/blob-images-photos-prod/stream/2020-11-01T18:41:37.291'
 });
 
 module.exports.frontend = () => ({
-    'dev': 'http://localhost:3000',
-    'prod': 'clubalmanac.com'
+    'dev': 'https://localhost:3000',
+    'prod': 'https://clubalmanac.com'
 });
 
 module.exports.webmaster = () => ({
